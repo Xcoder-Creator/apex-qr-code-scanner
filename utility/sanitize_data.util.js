e77aa2d3a7b9959226252d81cd1bfa50fe51b6ea
Xcoder-Creator <59529270+Xcoder-Creator@users.noreply.github.com>
2024-04-18 23:55:33 +0100

    Add files via upload

---
+++ utility/sanitize_data.util.js
@@ -0,0 +1,6 @@
+//Sanitize data
+let sanitize_data = (data) => {
+    return data.split('"').join('').replace(/^[ ]+|[ ]+$/g, '').trim(); //Return sanitized data
+}
+
+module.exports = sanitize_data;
